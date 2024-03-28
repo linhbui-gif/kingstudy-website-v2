ba18d69eca44b2d086f4fe92c1f8ff417cba0e19
quocdat <quocdattranhuu1606@gmail.com>
2024-03-28 16:39:00 +0700

    build roadmap

--- src/pages/design/index.js
+++ src/pages/design/index.js
@@ -4,38 +4,70 @@ import {EIconName} from "@/components/Icon/Icon.enum";
 import ButtonComponent from "@/components/Button";
 import {EStyleButton} from "@/components/Button/Button.enum";
 
+import Image from "next/image";
+
 const Design = () => {
   return (
-    <div className="flex justify-center p-[20px] bg-black">
-        <ButtonComponent
-          className={'flex justify-center gap-2 items-center w-[21.6rem] h-[4.8rem]'}
-          bgUrlButton={'/images/image-border-button.png'}
-          hasIcon={true}
-        />
-        <ButtonComponent
-          className={'w-[38.7rem] h-[8.6rem]'}
-          bgUrlButton={'/images/image-border-gray-button.png'}
-          styleButton={EStyleButton.CommingSoon}
-        />
-        <Icon name={EIconName.Discord_Small}/>
-        <Icon name={EIconName.Facebook}/>
-        <Icon name={EIconName.Twitter}/>
-        <Icon name={EIconName.Twitter_Small}/>
-        <Icon name={EIconName.Instagram}/>
-        <Icon name={EIconName.FitGPT}/>
-        <Icon name={EIconName.Sensor}/>
-        <Icon name={EIconName.Plan}/>
-        <Icon name={EIconName.Diet}/>
-        <Icon name={EIconName.Wallet}/>
-        <Icon name={EIconName.Telegram}/>
-        <Icon name={EIconName.Discord}/>
-        <Icon name={EIconName.Apple}/>
-        <Icon name={EIconName.Boots}/>
-        <Icon name={EIconName.Clock}/>
-        <Icon name={EIconName.Dumbbell}/>
-        <Icon name={EIconName.MenuBar}/>
-        <Icon name={EIconName.Flash}/>
-    </div>
+     <div className="bg-black  bg-center bg-no-repeat bg-cover lg:bg-[length:100%_100%] pt-[20px] pb-[60px] lg:pt-[10px] lg:pb-[70px] h-[100vh]">
+        <div className="w-full max-w-[1024px] mx-auto px-4">
+            <div className="flex flex-col items-center justify-center max-w-[1080px] m-auto relative">
+                <Image src="/images/image-roadmap.svg" alt="Roadmap" width={419} height={346} className="relative z-[30]"/>
+                <Image src="/images/image-total.svg" alt="Total" width={820} height={196} className="absolute z-[20] top-[73%]"/>
+            </div>
+            <div className="flex flex-col lg:flex-row items-center lg:items-start justify-between w-full relative lg:-mt-[91px] gap-[30px] lg:gap-0 ">
+               <div className=" absolute left-[-5%] top-[4.2rem] w-[269px] min-h-[269px] h-auto lg:w-[279px] bg-[url('/images/images-roadmap-item.svg')] bg-center bg-no-repeat bg-[length:100%_100%]  p-10" >
+                   <div className="absolute w-[94%] left-[3%] top-[27%] bg-white z-10 rounded-[2.8rem] h-full py-[4rem] z-[40px]">
+                       <p className=" absolute text-[#29282D4D] z-[3] -right-3 bottom-16 rotate-90 text-[30px] font-semibold">PHASE 01</p>
+                       <ul className="list-disc font-ubuntu list-inside">
+                           <li className="text-[#636363] text-[1.6rem]">Team Building</li>
+                           <li className="text-[#636363] text-[1.6rem]">AI Training</li>
+                           <li className="text-[#636363] text-[1.6rem]">Market Research</li>
+                           <li className="text-[#636363] text-[1.6rem]">Design Website</li>
+                           <li className="text-[#636363] text-[1.6rem]">Design System & UI Kits</li>
+                           <li className="text-[#636363] text-[1.6rem]">Mockup Design</li>
+                           <li className="text-[#636363] text-[1.6rem]">App Mechanisms & Documents</li>
+                           <li className="text-[#636363] text-[1.6rem]">IOS & Android MVP finished</li>
+                       </ul>
+                   </div>
+               </div>
+                <div className="absolute left-[34%] top-[9.9rem] w-[269px] min-h-[269px]  h-auto  bg-[url('/images/images-roadmap-item.svg')] bg-center bg-no-repeat bg-[length:100%_100%]  p-10">
+                    <div className="absolute w-[94%] left-[3%] top-[27%] bg-white z-10 rounded-[2.8rem] h-full py-[4rem] z-[40px]">
+                        <p className=" absolute text-[#29282D4D] z-[3] -right-3 bottom-16 rotate-90 text-[30px] font-semibold">PHASE 02</p>
+                        <ul className="list-disc font-ubuntu list-inside">
+                                    <li className="text-[#636363] text-[1.6rem]">Private Test</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Community Building</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Fund Raising</li>
+                                    <li className="text-[#636363] text-[1.6rem]">App Launch</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Affiliate Program</li>
+                                    <li className="text-[#636363] text-[1.6rem]">IDO</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Token Launch</li>
+                                    <li className="text-[#636363] text-[1.6rem]">NFT Marketplace</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Defi System</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="absolute w-[269px] min-h-[269px] left-[72%] top-[4.2rem] h-auto bg-[url('/images/images-roadmap-item.svg')]  bg-center bg-no-repeat bg-[length:100%_100%]  p-10  ">
+                    <div className="absolute w-[94%] left-[3%] top-[27%] bg-white z-10 rounded-[2.8rem] h-full py-[4rem] z-[4rem]">
+                        <p className=" absolute text-[#29282D4D] z-[3] -right-3 bottom-16 rotate-90 text-[30px] font-semibold">PHASE 03</p>
+                        <ul className="list-disc font-ubuntu list-inside">
+                                    <li className="text-[#636363] text-[1.6rem]">Daily Missions</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Mini Games</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Achievement System</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Leaderboard Features</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Group / Guild Features</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Social Features</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Community Events</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Dating Features</li>
+                                    <li className="text-[#636363] text-[1.6rem]">PVP Features</li>
+                                    <li className="text-[#636363] text-[1.6rem]">Badges System</li>
+                                    <li className="text-[#636363] text-[1.6rem]">DAO Model</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+     </div>
   );
 };
 
