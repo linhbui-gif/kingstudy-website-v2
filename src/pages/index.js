3fce1457f61795bb14de9d323bf4b3cb36bf382b
Linh BUi <linhbq68@wru.vn>
2024-05-08 01:01:41 +0700

    134-kingstudy_ui-button-component:Done component logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import CardSkeleton from '@/components/Card/CardSkeleton';
 import Meta from '@/components/Meta';
 import TopBar from '@/containers/Topbar';
 import { getListSchool } from '@/services/school';
+import ButtonComponent from "@/components/Button";
+import {EIconName} from "@/components/Icon/Icon.enum";
 
 export default function Home() {
   const [data, setData] = useState([]);
@@ -40,32 +42,6 @@ export default function Home() {
     <div className={`min-h-screen`}>
       <Meta title="KingStudy" />
       <TopBar />
-      {/*<Row gutter={[24, 24]} className={'flex'}>*/}
-      {/*  {data &&*/}
-      {/*    data.map((element) => {*/}
-      {/*      return (*/}
-      {/*        <Col span={24} md={{ span: 6 }} key={element?.id}>*/}
-      {/*          {loading ? LoadingSkeletonCards() : ''}*/}
-      {/*          {!loading && (*/}
-      {/*            <Card*/}
-      {/*              url={'https://kingstudy.vn' + element?.logo}*/}
-      {/*              title={element?.name}*/}
-      {/*              description={element?.heading}*/}
-      {/*              alt={element?.name}*/}
-      {/*            />*/}
-      {/*          )}*/}
-      {/*        </Col>*/}
-      {/*      );*/}
-      {/*    })}*/}
-      {/*  <Col span={24}>*/}
-      {/*    <div className={'w-full flex items-center justify-center'}>*/}
-      {/*      {' '}*/}
-      {/*      <Button type={'primary'} onClick={onLoadMore}>*/}
-      {/*        Load more*/}
-      {/*      </Button>*/}
-      {/*    </div>*/}
-      {/*  </Col>*/}
-      {/*</Row>*/}
     </div>
   );
 }
