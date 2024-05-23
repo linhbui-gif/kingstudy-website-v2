e1c45dec15dea4d6e4a8849067008d83cae82572
Linh BUi <linhbq68@wru.vn>
2024-05-24 01:06:11 +0700

    refactor(fonts): Refactor import font local

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,8 +6,80 @@ export default function Document() {
   return (
     <Html lang={AppConfig.locale}>
       <Head>
+        <link
+          rel="preload"
+          href="assets/fonts/BeVietnamPro-Thin.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/BeVietnamPro-ExtraLight.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/BeVietnamPro-Light.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/BeVietnamPro-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/BeVietnamPro-Medium.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/BeVietnamPro-SemiBold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/BeVietnamPro-Bold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/BeVietnamPro-ExtraBold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/BeVietnamPro-Black.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+
         <link rel="preload" as="style" href="/assets/font.css" />
         <link rel="stylesheet" href="/assets/font.css" />
       </Head>
