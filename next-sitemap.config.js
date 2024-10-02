dd86d2e227c6a0f93d4445e0c143c020d4994c4d
Linh BUi <linhbq68@wru.vn>
2024-10-03 01:54:07 +0700

    feat(sitemap): Update sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -15,18 +15,41 @@ module.exports = {
   ],
 
   additionalPaths: async () => {
-    const posts = await axios.get('https://admin.kingstudy.vn/api/v1/blog'); // Gọi API để lấy danh sách bài viết
-    const data = posts?.data;
+    try {
+      const postsResponse = await axios.get(
+        'https://admin.kingstudy.vn/api/v1/blog'
+      );
+      const postsData = postsResponse?.data?.data;
 
-    const paths = data?.length
-      ? data.map((post) => ({
-          loc: `/tin-tuc/${post.alias}`,
-          lastmod: post.updated_at || new Date().toISOString(),
-          changefreq: 'weekly',
-          priority: 0.8,
-        }))
-      : [];
+      const schoolsResponse = await axios.get(
+        'https://admin.kingstudy.vn/api/v1/school?limit=500'
+      );
+      const schoolsData = schoolsResponse?.data?.data?.data;
 
-    return paths;
+      // Tạo paths từ bài viết
+      const postPaths = postsData?.length
+        ? postsData.map((post) => ({
+            loc: `/tin-tuc/${post.alias}`,
+            lastmod: post.updated_at || new Date().toISOString(),
+            changefreq: 'daily',
+            priority: 0.8,
+          }))
+        : [];
+
+      // Tạo paths từ trường học
+      const schoolPaths = schoolsData?.length
+        ? schoolsData.map((school) => ({
+            loc: `/truong-hoc/${school.slug}`, // Giả sử trường có thuộc tính 'alias'
+            lastmod: school.updated_at || new Date().toISOString(),
+            changefreq: 'daily',
+            priority: 0.8,
+          }))
+        : [];
+
+      // Kết hợp các paths từ bài viết và trường học
+      return [...postPaths, ...schoolPaths];
+    } catch (error) {
+      return []; // Trả về mảng rỗng nếu có lỗi
+    }
   },
 };
